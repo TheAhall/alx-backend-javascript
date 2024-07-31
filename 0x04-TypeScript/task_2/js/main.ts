ab944f266433efa8d08ed8c291021d56bfa55068
TheAhall <81965922+TheAhall@users.noreply.github.com>
2024-07-31 23:59:07 +0100

    0x04-TypeScript

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -9,30 +9,70 @@ interface TeacherInterface {
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
-  
+
 class Director implements DirectorInterface {
-  workFromHome = () : string => 'Working from home';
-  getCoffeeBreak = () : string => 'Getting a coffee break';
-  workDirectorTasks = () : string => 'Getting to director tasks';
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
 }
 
 class Teacher implements TeacherInterface {
-  workFromHome = () : string => 'Cannot work from home';
-  getCoffeeBreak = () : string => 'Cannot have a break';
-  workTeacherTasks = () : string => 'Getting to work';
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
 }
 
-export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
-
-export const isDirector = (employee: Teacher | Director): boolean => employee instanceof Director;
-
-export const executeWork = (employee: Teacher | Director): void => {
-  const work = employee instanceof Teacher? employee.workTeacherTasks() : employee.workDirectorTasks();
-  console.log(work)
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
 }
 
-type Subjects = 'Math' | 'History';
-
-export const teachClass = (todayClass: Subjects) : string => {
-  return todayClass === 'Math' ? 'Teaching Math': 'Teaching History';
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
 }
+
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+const employee1 = createEmployee(200);
+const employee2 = createEmployee(1000);
+
+executeWork(employee1); // Getting to work
+executeWork(employee2); // Getting to director tasks
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
+  }
+}
+
+console.log(teachClass("Math")); // Teaching Math
+console.log(teachClass("History")); // Teaching History
